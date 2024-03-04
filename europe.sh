bf1713988ae09a4d5c48a7c4ffdf77e5a28ff7de
Neil Fitzpatrick <neil@viavoom>
2024-03-04 14:33:03 +0000

    Tidy up and add basic shell script

---
+++ europe.sh
@@ -0,0 +1,1 @@
+tilemaker --input /media/neil/sata/maps/data/europe-latest.osm.pbf --config config-tmv3.json --process process.lua --shard-stores --store ./store --output /media/neil/sata/maps/data/europe.mbtiles
